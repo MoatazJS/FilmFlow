4eca240cf766d585f73d8461ef059c8eafd41148
Moataz <moataz_abdullah@outlook.com>
2025-10-03 00:42:10 +0300

    created detailspage header

--- src/app/(pages)/movie-details/[id]/page.tsx
+++ src/app/(pages)/movie-details/[id]/page.tsx
@@ -1,3 +1,4 @@
+import DetailsHeader from "@/components/DetailsPageComponents/DetailsHeader";
 import { getMovieDetails } from "@/lib/services/ApiServices";
 import React from "react";
 
@@ -12,7 +13,9 @@ export default async function MovieDetailsPage({
   return (
     <>
       <main className="min-h-screen flex flex-col bg-gradient-to-b from-black via-zinc-900 to-black text-gray-100">
-        <h1 className="text-center text-6xl text-yellow-500">
+        <DetailsHeader movie={movie} />
+
+        <h1 className="mt-48 text-center text-6xl text-yellow-500">
           HI DETAILS PAGE
         </h1>
       </main>
--- src/components/DetailsPageComponents/DetailsHeader.tsx
+++ src/components/DetailsPageComponents/DetailsHeader.tsx
@@ -1,5 +1,43 @@
+import { MovieDetailsResponse } from "@/lib/interfaces/interface";
+import Image from "next/image";
 import React from "react";
 
-export default function DetailsHeader() {
-  return <div>DetailsHeader</div>;
+export default function DetailsHeader({
+  movie,
+}: {
+  movie: MovieDetailsResponse;
+}) {
+  return (
+    <>
+      <section className="relative w-full h-[400px]">
+        {/* Cover / Backdrop */}
+        <Image
+          src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
+          alt={movie.title}
+          fill
+          priority
+          className="object-cover"
+        />
+
+        {/* Dark overlay */}
+        <div className="absolute inset-0 bg-black/60" />
+
+        {/* Poster as profile picture */}
+        <div className="absolute inset-x-0 bottom-[-120px] flex justify-center">
+          <div className="flex flex-col items-center">
+            <Image
+              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+              alt={movie.title}
+              width={200}
+              height={300}
+              className="rounded-xl shadow-xl border-4 border-zinc-900"
+            />
+            <h1 className="mt-4 text-3xl font-bold text-yellow-500 text-center">
+              {movie.title}
+            </h1>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
