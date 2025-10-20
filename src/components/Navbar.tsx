a336ca82fabb0d2177b1bd1fd3869e4e7c133cb9
Moataz <moataz_abdullah@outlook.com>
2025-10-20 22:22:01 +0300

    making navbar responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,9 @@ import {
   NavigationMenuList,
   NavigationMenuLink,
 } from "@/components/ui/navigation-menu";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { Button } from "@/components/ui/button";
+import { Menu } from "lucide-react";
 
 export default function Navbar() {
   return (
@@ -16,55 +18,116 @@ export default function Navbar() {
         <Link href="/" className="text-2xl font-extrabold text-yellow-400">
           FilmFlow
         </Link>
-
-        <NavigationMenu>
-          <NavigationMenuList>
-            <NavigationMenuItem>
-              <NavigationMenuLink asChild>
-                <Link className="text-gray-300 hover:text-yellow-400" href="/">
-                  Home
-                </Link>
-              </NavigationMenuLink>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <NavigationMenuLink asChild>
-                <Link
-                  className="text-gray-300 hover:text-yellow-400"
-                  href="/popular"
-                >
-                  Popular
-                </Link>
-              </NavigationMenuLink>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <NavigationMenuLink asChild>
-                <Link
-                  className="text-gray-300 hover:text-yellow-400"
-                  href="/top-rated"
-                >
-                  Top Rated
-                </Link>
-              </NavigationMenuLink>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <NavigationMenuLink asChild>
-                <Link
-                  className="text-gray-300 hover:text-yellow-400"
-                  href="/upcoming"
-                >
-                  Upcoming
-                </Link>
-              </NavigationMenuLink>
-            </NavigationMenuItem>
-          </NavigationMenuList>
-        </NavigationMenu>
-
-        <Button
-          variant="outline"
-          className="border-yellow-400 text-yellow-400 hover:bg-yellow-400 hover:text-black transition"
-        >
-          Search
-        </Button>
+        <div className="hidden md:flex items-center gap-6">
+          <NavigationMenu>
+            <NavigationMenuList>
+              <NavigationMenuItem>
+                <NavigationMenuLink asChild>
+                  <Link
+                    className="text-gray-300 hover:text-yellow-400"
+                    href="/"
+                  >
+                    Home
+                  </Link>
+                </NavigationMenuLink>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuLink asChild>
+                  <Link
+                    className="text-gray-300 hover:text-yellow-400"
+                    href="/popular"
+                  >
+                    Popular
+                  </Link>
+                </NavigationMenuLink>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuLink asChild>
+                  <Link
+                    className="text-gray-300 hover:text-yellow-400"
+                    href="/top-rated"
+                  >
+                    Top Rated
+                  </Link>
+                </NavigationMenuLink>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuLink asChild>
+                  <Link
+                    className="text-gray-300 hover:text-yellow-400"
+                    href="/upcoming"
+                  >
+                    Upcoming
+                  </Link>
+                </NavigationMenuLink>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
+        </div>
+        <div className="flex items-center justify-end gap-4">
+          <div className="md:hidden">
+            <Sheet>
+              <SheetTrigger className="text-yellow-500 cursor-pointer ">
+                <Menu />
+              </SheetTrigger>
+              <SheetContent
+                side="right"
+                className="bg-gray-900 w-[200px] min-h-screen"
+              >
+                <NavigationMenu>
+                  <NavigationMenuList className="flex flex-col justify-start items-start min-h-screen mt-10">
+                    <NavigationMenuItem>
+                      <NavigationMenuLink asChild>
+                        <Link
+                          className="text-gray-300 hover:text-yellow-400"
+                          href="/"
+                        >
+                          Home
+                        </Link>
+                      </NavigationMenuLink>
+                    </NavigationMenuItem>
+                    <NavigationMenuItem>
+                      <NavigationMenuLink asChild>
+                        <Link
+                          className="text-gray-300 hover:text-yellow-400"
+                          href="/popular"
+                        >
+                          Popular
+                        </Link>
+                      </NavigationMenuLink>
+                    </NavigationMenuItem>
+                    <NavigationMenuItem>
+                      <NavigationMenuLink asChild>
+                        <Link
+                          className="text-gray-300 hover:text-yellow-400"
+                          href="/top-rated"
+                        >
+                          Top Rated
+                        </Link>
+                      </NavigationMenuLink>
+                    </NavigationMenuItem>
+                    <NavigationMenuItem>
+                      <NavigationMenuLink asChild>
+                        <Link
+                          className="text-gray-300 hover:text-yellow-400"
+                          href="/upcoming"
+                        >
+                          Upcoming
+                        </Link>
+                      </NavigationMenuLink>
+                    </NavigationMenuItem>
+                  </NavigationMenuList>
+                </NavigationMenu>
+              </SheetContent>
+            </Sheet>
+          </div>
+          <Button
+            variant="outline"
+            className="border-yellow-400 text-yellow-400 hover:bg-yellow-400 hover:text-black transition"
+          >
+            Search
+          </Button>
+        </div>
       </div>
     </header>
   );
