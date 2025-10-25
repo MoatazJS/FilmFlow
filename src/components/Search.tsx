445a592c12cf1fa6d4c00384851d3fc7e5dbca4a
Moataz <moataz_abdullah@outlook.com>
2025-10-25 22:26:03 +0300

    finished search functionality

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,25 +1,47 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Input } from "@/components/ui/input";
 import { searchMovies } from "@/lib/services/ApiServices";
+import { Movie } from "@/lib/interfaces/interface";
 
 export default function Search() {
   const [query, setQuery] = useState<string>("");
+  const [results, setResults] = useState<Movie[]>([]);
+  const [resultError, setResultError] = useState<string>("");
 
   async function getSearchResults(query: string) {
+    if (!query.trim()) {
+      setResults([]);
+      setResultError("");
+      return;
+    }
     const res = await searchMovies(query);
-    console.log(res);
+    if (res.length == 0) {
+      setResultError("We found 0 matches");
+      setResults([]);
+    } else {
+      setResultError("");
+      setResults(res);
+      console.log(res);
+    }
   }
 
   function handleSearchInput(e: React.ChangeEvent<HTMLInputElement>) {
     const value = e.target.value;
     setQuery(value);
-    getSearchResults(value);
   }
+
+  useEffect(() => {
+    const delay = setTimeout(() => {
+      getSearchResults(query);
+    }, 500);
+    return () => clearTimeout(delay);
+  }, [query]);
 
   return (
     <div className="bg-gray-50 text-black ">
       <Input
+        className="w-40 md:w-52 text-[12px] md:text-[14px]"
         onChange={handleSearchInput}
         id="searchInput"
         type="text"
